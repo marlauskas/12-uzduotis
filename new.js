fc42258f2c8f44ef150bdb7f4d611dad382433d5
marlauskas <modesto.arlausko@gmail.com>
2022-07-18 20:55:28 +0300

    java

---
+++ new.js
@@ -0,0 +1,29 @@
+//Pirma uzduotis
+// var kaina = prompt("Iveskite prekes kaina");
+// var kaina = (kaina) * ((100 + 21) /100);
+// var pvm = kaina * 21 / 100;
+// var Number = Number(kaina);
+// var bepvm = (kaina) * ((100 - 21) /100);
+// console.log("Kaina su PVM yra: " + kaina + "€");
+// console.log("PVM yra: " + pvm + "€");
+// console.log("Kaina be PVM yra " + bepvm + "€")
+
+
+//Antra uzduotis
+var names = ["John Snow", "Cersei Lannister", "Daenerys Targaryen", "Theon Greyjoy" , "Tyrion Lannister", "Arya Stark"];
+console.log(names[0]);
+console.log(names[5]);
+var names = prompt ("Iveskite skaiciu nuo 1 iki 6");
+if (names==1){
+    console.log("John Snow")
+} else if (names==2)
+    console.log("Cersei Lannister")
+    else if (names==3)
+    console.log("Daenerys Targaryen")
+     else if (names==4)
+    console.log("Theon Greyjoy")
+     else if (names==5)
+    console.log("Tyrion Lannister")
+     else if (names==6)
+    console.log("Arya Stark")
+
